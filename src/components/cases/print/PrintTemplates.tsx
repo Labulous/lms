cd41a35a96e625b8635a8abcf7fc531b5ab6ba44
zahidh1626 <zahid.nagri.edu.pk@gmail.com>
2025-01-23 00:07:59 +0500

    updated

--- src/components/cases/print/PrintTemplates.tsx
+++ src/components/cases/print/PrintTemplates.tsx
@@ -193,7 +193,6 @@ export const QRCodeTemplate: React.FC<PrintTemplateProps> = ({
   </div>
 );
 
-
 export const InvoiceTemplate: React.FC<PrintTemplateProps> = ({
   caseDetails,
 }) => (
@@ -202,120 +201,129 @@ export const InvoiceTemplate: React.FC<PrintTemplateProps> = ({
       return (
         <div
           key={index}
+          className="min-h-[277mm] w-[180mm] mx-auto bg-white"
           style={{
             height: "277mm", // Fixed height to match LETTER page size
           }}
         >
-          <div className="p-0">
-            {/* Header Section */}
-            <div className="flex justify-between mb-8">
-              {/* Company Info */}
-              <div className="flex flex-col gap-0 items-start">
-                <img
-                  src={staticLabLogo}
-                  alt="Solaris Dental Design Logo"
-                  width={120}
-                  height={120}
-                  className="object-contain flex justify-center items-center"
-                />
-                <div className="text-sm font-medium">
-                  <h3 className="font-bold mb-1 text-xl">
-                    {invoice.labDetail?.name}
-                  </h3>
-                  <p>{invoice.labDetail?.office_address.address_1}</p>
-                  <p>{invoice.labDetail?.office_address.address_2}</p>
-                  <p>
-                    <span>{invoice.labDetail?.office_address.city}</span>,{" "}
-                    {invoice.labDetail?.office_address.state_province}{" "}
-                    <span>{invoice.labDetail?.office_address.zip_postal}</span>
-                  </p>
-                  <p>{invoice.labDetail?.office_address.phone_number}</p>
+          <div className="border border-gray-800">
+            <div className="p-5">
+              <div className="p-0">
+                {/* Header Section */}
+                <div className="flex justify-between mb-8">
+                  {/* Company Info */}
+                  <div className="flex flex-col gap-0 items-start">
+                    <img
+                      src={staticLabLogo}
+                      alt="Solaris Dental Design Logo"
+                      width={120}
+                      height={120}
+                      className="object-contain flex justify-center items-center"
+                    />
+                    <div className="text-sm font-medium">
+                      <h3 className="font-bold mb-1 text-xl">
+                        {invoice.labDetail?.name}
+                      </h3>
+                      <p>{invoice.labDetail?.office_address.address_1}</p>
+                      <p>{invoice.labDetail?.office_address.address_2}</p>
+                      <p>
+                        <span>{invoice.labDetail?.office_address.city}</span>,{" "}
+                        {invoice.labDetail?.office_address.state_province}{" "}
+                        <span>
+                          {invoice.labDetail?.office_address.zip_postal}
+                        </span>
+                      </p>
+                      <p>{invoice.labDetail?.office_address.phone_number}</p>
+                    </div>
+                  </div>
+
+                  {/* Invoice Details */}
+                  <div className="text-sm">
+                    <h1 className="text-xl font-bold mb-2">INVOICE</h1>
+                    <p className="font-bold">
+                      <p className="text-gray-600">
+                        Inv #:{" "}
+                        {invoice.case_number
+                          ? invoice.case_number.split("-")[2]
+                          : "N/A"}
+                      </p>
+                    </p>
+                    <p className="font-bold">
+                      {formatDate(invoice?.created_at || "1/7/2025")}
+                    </p>
+                    <div className="mt-4 font-medium">
+                      <p className="font-bold">Ship To:</p>
+                      <p>{invoice.client.street}</p>
+                      <p>
+                        {invoice.client.city}
+                        {", "}
+                        <span>{invoice.client.state}</span>
+                      </p>
+                      <p>{invoice.client.phone}</p>
+                    </div>
+                  </div>
                 </div>
-              </div>
 
-              {/* Invoice Details */}
-              <div className="text-sm">
-                <h1 className="text-xl font-bold mb-2">INVOICE</h1>
-                <p className="font-bold">
-                  <p className="text-gray-600">
-                    Inv #:{" "}
-                    {invoice.case_number
-                      ? invoice.case_number.split("-")[2]
-                      : "N/A"}
+                {/* Patient Section */}
+                <div className="mb-6">
+                  <p className="font-medium">
+                    <span className="font-bold">Patient:</span>{" "}
+                    {invoice?.patient_name}
                   </p>
-                </p>
-                <p className="font-bold">
-                  {formatDate(invoice?.created_at || "1/7/2025")}
-                </p>
-                <div className="mt-4 font-medium">
-                  <p className="font-bold">Ship To:</p>
-                  <p>{invoice.client.street}</p>
-                  <p>
-                    {invoice.client.city}
-                    {", "}
-                    <span>{invoice.client.state}</span>
-                  </p>
-                  <p>{invoice.client.phone}</p>
                 </div>
-              </div>
-            </div>
 
-            {/* Patient Section */}
-            <div className="mb-6">
-              <p className="font-medium">
-                <span className="font-bold">Patient:</span>{" "}
-                {invoice?.patient_name}
-              </p>
-            </div>
+                {/* Services Table */}
+                <div className="mb-8">
+                  <div className="grid grid-cols-4 border-b border-gray-800 pb-2 mb-4">
+                    <h2 className="font-bold">Description</h2>
+                    <h2 className="font-bold text-right">Dicount %</h2>
+                    <h2 className="font-bold text-right">Amount</h2>
+                    <h2 className="font-bold text-right">Final Amount</h2>
+                  </div>
 
-            {/* Services Table */}
-            <div className="mb-8">
-              <div className="grid grid-cols-4 border-b border-gray-800 pb-2 mb-4">
-                <h2 className="font-bold">Description</h2>
-                <h2 className="font-bold text-right">Dicount %</h2>
-                <h2 className="font-bold text-right">Amount</h2>
-                <h2 className="font-bold text-right">Final Amount</h2>
-              </div>
-
-              {invoice?.products.map((item, index) => {
-                return (
-                  <div className="grid grid-cols-4 text-sm">
-                    <div key={index} className="space-y-4 font-medium">
-                      <div>
-                        <p>{item.name}</p>
-                        <p className="text-sm">
-                          Teeth: #{item.teethProduct?.tooth_number[0]}
+                  {invoice?.products.map((item, index) => {
+                    return (
+                      <div className="grid grid-cols-4 text-sm">
+                        <div key={index} className="space-y-4 font-medium">
+                          <div>
+                            <p>{item.name}</p>
+                            <p className="text-sm">
+                              Teeth: #{item.teethProduct?.tooth_number[0]}
+                            </p>
+                          </div>
+                        </div>
+                        <p className="text-right">
+                          {item.discounted_price.discount}%
+                        </p>
+                        <p className="text-right">
+                          ${item.discounted_price.price}
+                        </p>
+                        <p className="text-right">
+                          ${item.discounted_price.final_price}
                         </p>
                       </div>
-                    </div>
-                    <p className="text-right">
-                      {item.discounted_price.discount}%
-                    </p>
-                    <p className="text-right">${item.discounted_price.price}</p>
-                    <p className="text-right">
-                      ${item.discounted_price.final_price}
-                    </p>
-                  </div>
-                );
-              })}
+                    );
+                  })}
 
-              <div className="flex justify-between items-center pt-4 mt-4 border-t border-gray-800">
-                <p className="text-sm">J4</p>
-                <div className="flex flex-col gap-2 items-end">
-                  <p className="font-bold">
-                    Total: ${invoice?.invoice?.[0]?.amount}
-                  </p>
-                  <p className="font-bold">
-                    Total Due: ${invoice?.invoice?.[0]?.due_amount}
-                  </p>
+                  <div className="flex justify-between items-center pt-4 mt-4 border-t border-gray-800">
+                    <p className="text-sm">J4</p>
+                    <div className="flex flex-col gap-2 items-end">
+                      <p className="font-bold">
+                        Total: ${invoice?.invoice?.[0]?.amount}
+                      </p>
+                      <p className="font-bold">
+                        Total Due: ${invoice?.invoice?.[0]?.due_amount}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                {/* Footer */}
+
+                <div className="text-center mt-4 font-medium">
+                  <p className="text-sm">Thank you for your business!</p>
                 </div>
               </div>
-            </div>
-
-            {/* Footer */}
-
-            <div className="text-center mt-4 font-medium">
-              <p className="text-sm">Thank you for your business!</p>
             </div>
           </div>
         </div>
@@ -763,7 +771,9 @@ export const LabSlipTemplate: React.FC<PrintTemplateProps> = ({
                 <Header caseDetail={item} />
                 <div className="border-2" />
 
-                <div className={`grid grid-cols-${cases.length} gap-5 space-y-5 my-5`}>
+                <div
+                  className={`grid grid-cols-${cases.length} gap-5 space-y-5 my-5`}
+                >
                   {item.products.map((teeth, index) => {
                     return (
                       <TeetDetail
